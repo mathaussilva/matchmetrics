e4b5c4c88bbe4136c43c05589945e266bac30678
mathaussilva <mathaussilva7@gmail.com>
2024-07-25 18:54:08 -0300

    Update ThreadCard.tsx

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -144,7 +144,7 @@ function ThreadCard({
         </div>
       )}
 
-      {!isComment && (
+      {!isComment && community && (
         <Link
           href={`/communities/${community.id}`}
           className='mt-5 flex items-center'
