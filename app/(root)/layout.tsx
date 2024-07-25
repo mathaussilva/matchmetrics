289c9364b8db31bf9c1ca62766eb1629ff7b251e
mathaussilva <mathaussilva7@gmail.com>
2024-07-25 19:21:34 -0300

    apple-icon commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -31,6 +31,21 @@ export default function RootLayout({
       }}
     >
       <html lang='en'>
+        <head>
+          <link rel="icon" href="/favicon.ico" sizes="any" />
+          <link
+            rel="icon"
+            href="/icon?<generated>"
+            type="image/<generated>"
+            sizes="<generated>"
+          />
+          <link
+            rel="apple-touch-icon"
+            href="/apple-icon?<generated>"
+            type="image/<generated>"
+            sizes="<generated>"
+          />  
+        </head>
         <body className={inter.className}>
           <Topbar />
 
